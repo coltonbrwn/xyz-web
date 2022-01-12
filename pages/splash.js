50862de51ddfddb4b4e06e61c14b81ee19a2c3be
Colton <colton@coltonbrown.co>
2022-01-12 13:20:16 -0500

    basic scaffold

---
+++ pages/splash.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css'
+import React from 'react'
+
+export default class Splash extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <p>HI</p>
+      </div>
+    )
+  }
+}
